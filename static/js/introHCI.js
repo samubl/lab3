0d4168da87d9fad9845483db2da36a36394411b9
Samuel Blake <samuelblake97@gmail.com>
2019-01-24 10:32:03 -0800

    Worked on Lab 3
    Worked on reacting to user input.

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,30 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron p").toggleClass("active");
+		$("#testjs").text("Experience Chan! It's not a lot of questions." + 
+		" Too many questions is the Chan disease. " + 
+		" The best way is just to observe the noise of the world." + 
+		" The answer to your questions? Ask your own heart.");
 	});
+	$("a.thumbnail").click(projectClick);
+}
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+// Add any additional listeners here
+// example: $("#div-id").click(functionToCall);
+
+function projectClick(e) {
+	// prevent the page from reloading    
+	console.log("Project Click");  
+	e.preventDefault();
+	// In an event handler, $(this) refers to      
+	// the object that triggered the event      
+	$(this).css("background-color", "#E317FF");
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	 } else {
+		description.fadeOut();
+	 }
 }
